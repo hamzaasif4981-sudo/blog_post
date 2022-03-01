3f9907267de82fabe99b554ef9bd0a7c4072f1da
hamzaasif4981-sudo <hamzaasif4981@gmail.com>
2022-03-01 18:50:59 +0500

    Add new blog functionality

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,9 +7,16 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
   end
 
   def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "Blog was successfuly created"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -20,4 +27,9 @@ class BlogsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:name, :short_description, :post)
+    end
 end
